30b467d63fc99dfb8592c9e1f3d7fd1bdb7b4732
neo-kjb <anaszawahreh.dev@gmail.com>
2022-11-28 18:49:12 +0300

    Collecting test files

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log('run');
+const Runner = require('./runner')
+const runner = new Runner()
+
+const run = async () => {
+  await runner.collectFiles(process.cwd())
+  console.log(runner.testFiles)
+}
+
+run()
